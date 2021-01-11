05d5b2fd3ac215323a65819bfda62836bbadda8f
Matt Gross <mbgross111@gmail.com>
2021-01-10 21:22:00 -0800

    quiz finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,22 +34,39 @@ var qArr = [
     a: `"object"`
   },
   {
-    q: `which of the following is not a data type in Java Script?`,
+    q: `Which of the following is not a data type in Java Script?`,
     l: [`string`, `boolean`, `array`, `table`],
     a: `table`,
   },
   {
-    q:``,
-    l:[],
-    a:``,
+    q: `Using the setInterval() method will call a function until ______  is called?`,
+    l: [`setStop()`, `breakInterval()`, `clearInterval()`, `setTimeout()`],
+    a: `clearInterval()`,
   },
+  {
+    q: `Who created JavaScript?`,
+    l: [`Yan Zhu`,`Brendan Eich`,`Jim Highsmith`,`Håkon Wium Lie`],
+    a: `Brendan Eich`,
+  },
+  {
+    q: `Who created JavaScript?`,
+    l: [`Yan Zhu`,`Brendan Eich`,`Jim Highsmith`,`Håkon Wium Lie`],
+    a: `Brendan Eich`,
+  },
+  {
+    q: `Var arr = ["3", "2", "1", "0"]; <br>
+        var x = arr[2] + arr[0]; <br>
+        console.log(x) prints____?`,    
+    l: [`2`, `"20"`, `4` ,`"13"`],
+    a: `"13"`
+  }
 ];
 
 //~~~~~~~FUNCTIONS~~~~~~~
 
 var startQuiz = function() {
   //clear start message and button
-  var startMessage = document.querySelector(".start-message")
+  var startMessage = document.querySelector(".message")
   startMessage.remove();
 
   //start the timer
@@ -91,7 +108,6 @@ var askQuestion = function() {
   }
 }
 
-
 var scoreQuestion = function(event) {
   var userChoice = event.target;
   if(userChoice.matches("li.choice")) {
@@ -114,6 +130,9 @@ var scoreQuestion = function(event) {
 }
 
 var endQuiz = function(){
+  qEl.textContent = `Nice Job you answered ${score} questions correctly!`
+  var formEl = document.createElement("form");
+  
   console.log("lets end the game now. You scored: " + score)
 }
 
